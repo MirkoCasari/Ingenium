7131a093eeb41534524a36ac208bb19ab94b5586
MirkoCasari <135707123+MirkoCasari@users.noreply.github.com>
2025-09-09 11:01:02 +0000

    09.09.2025 finito

---
+++ test.py
@@ -0,0 +1,11 @@
+#%% Mirko Casari
+
+import time
+
+word = input("scegli una parola che devo indovinare ? ")
+
+for i in range(0,5):
+    print("...")
+    time.sleep(0.5)
+print("La tua parola è: ", word ) 
+print(":D")
